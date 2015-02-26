2e7192321682bce4b21dbd194366b29fa8373187
kulinichdenis <kulinichdenis@ukr.net>
2015-02-26 13:53:40 +0200

    test

--- src/ModuleStore.js
+++ src/ModuleStore.js
@@ -1,4 +1,5 @@
 function ModuleStore() {
+  // если вызвали без new
   if (!(this instanceof ModuleStore)) {
     return new ModuleStore();
   }
@@ -8,18 +9,31 @@ function ModuleStore() {
     return arguments.callee._singletonInstance;
 
   arguments.callee._singletonInstance = this;
+  this.store = {};
+//format - nameModule:{func:, depend:[]}
+
+}
+ModuleStore.prototype.addModule = function(name){
+    if(this.store[name] === undefined){
+        this.store[name] = {func: '', depends: ''};
+    }
 }
 
-ModuleStore.prototype.addModule = function(name, moduleFunction) {
+ModuleStore.prototype.addFunc = function(name, moduleFunction) {
+    this.store[name]['func'] = moduleFunction;
 }
 
 ModuleStore.prototype.getModule = function(name) {
+    return this.store[name];
 }
 
 ModuleStore.prototype.reset = function(name, moduleStore) {
+    this.store = {};
 }
 
-ModuleStore.prototype.contains = function(name) {
+ModuleStore.prototype.addDepends = function(name, depends) {
+    this.store[name]['depends'] = depends;
 }
+//var ModuleStore = ModuleStore();
 
 module.exports = ModuleStore;
--- src/module.js
+++ src/module.js
@@ -1,19 +1,51 @@
-// use ModuleStore if you want
-// var ModuleStore = require('./ModuleStore')();
+//use ModuleStore if you want
+var ModuleStore = require('./ModuleStore')();
 
-function Module(name, deps, moduleFunction) { }
+function Module(name, deps, moduleFunction) {
+    this.name = name;
+    this.deps = deps; // add
+    this.moduleFunction = moduleFunction;
+    ModuleStore.addModule(this.name);
+    if(this.moduleFunction || typeof(this.deps) === 'function') {
+        ModuleStore.addFunc(this.name, this.moduleFunction || this.deps);
+    }
+    if(typeof(this.deps) === 'object'){
+        ModuleStore.addDepends(this.name, this.deps);
+    }
+}
 
-Module.prototype.init = function() { };
+Module.prototype.init = function() {
+    var that = this;
+    var depenObject = {};
+    var depends = ModuleStore.getModule(this.name)['depends'];
+    if(depends){
+        depends.forEach(function(elem){
+            if (elem in that._cache){
+                depenObject[elem] = that._cache[elem];
+            }else{
+                depenObject[elem] = ModuleStore.getModule(elem)['func']();
+                that._cache[elem] = depenObject[elem];
+            }
+        });
+    }
+    var moduleFunction = ModuleStore.getModule(this.name)['func'];
+    if(moduleFunction){
+        moduleFunction.call(depenObject, depenObject);
+    }
+};
 
+Module.prototype._cache = {};
 
 function m(name, deps, moduleFunction) {
+    return new Module(name, deps, moduleFunction);
 }
+
 
 m.__reset__ = function() {
   // _modules = {}                   // resets module state
   //  or                             // only for testing purposes
-  // ModuleStore.reset();            // depends on your implementation
+  ModuleStore.reset();              // depends on your implementation
+  Module.prototype._cache = {};
 }
-
 
 module.exports = m;
