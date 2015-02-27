9dfefb29a75ebb80b03d7f55b16addff312ab7ba
kulinichdenis <kulinichdenis@ukr.net>
2015-02-27 15:59:57 +0200

    update

---
+++ src1/ModuleStore.js
@@ -0,0 +1,27 @@
+function ModuleStore() {
+    // если вызвали без new
+    if (!(this instanceof ModuleStore)) {
+        return new ModuleStore();
+    }
+    // must be a singleton
+    if (arguments.callee._singletonInstance)
+        return arguments.callee._singletonInstance;
+
+    arguments.callee._singletonInstance = this;
+    this.store = {};
+//format - nameModule:{func:}
+}
+
+ModuleStore.prototype.addModule = function(name, moduleFunction) {
+    this.store[name] = moduleFunction;
+}
+
+ModuleStore.prototype.getModule = function(name) {
+    return this.store[name];
+}
+
+ModuleStore.prototype.reset = function() {
+    this.store = {};
+}
+
+module.exports = ModuleStore;
