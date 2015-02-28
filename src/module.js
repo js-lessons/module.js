729ab067899e692fcd72a0b23c538c4f12d2249e
kulinichdenis <kulinichdenis@ukr.net>
2015-02-28 21:47:53 +0200

    update

--- src/module.js
+++ src/module.js
@@ -1,19 +1,62 @@
-// use ModuleStore if you want
-// var ModuleStore = require('./ModuleStore')();
+//use ModuleStore if you want
+var ModuleStore = require('./ModuleStore')();
 
-function Module(name, deps, moduleFunction) { }
+function Module(name, deps, moduleFunction) {
 
-Module.prototype.init = function() { };
+    this.name = name;
+    if(typeof(deps) === 'object'){
+        if(deps.length !== 0){
+            this.deps = deps;
+        }
+    }else{
+        this.moduleFunction = deps;
+    }
 
+    if(typeof(moduleFunction) === 'function'){
+        this.moduleFunction = moduleFunction;
+    }
+}
+
+Module.prototype.init = function() {
+
+    var that = this;
+    var depenObject = {};
+    var depends = this.deps;
+    if(depends){
+        depends.forEach(function(elem){
+            if (elem in that._cache){
+                depenObject[elem] = that._cache[elem];
+            }else{
+                depenObject[elem] = ModuleStore.getModule(elem).init();
+                that._cache[elem] = depenObject[elem];
+            }
+        });
+    }
+
+    if(this.moduleFunction){
+        return this.moduleFunction.call(depenObject, depenObject);
+    }
+};
+
+Module.prototype._cache = {};
 
 function m(name, deps, moduleFunction) {
+    if(deps || moduleFunction){
+        var instance = new Module(name, deps, moduleFunction);
+        ModuleStore.addModule(name, instance);
+        return instance;
+    }else{
+        return ModuleStore.getModule(name);
+    }
+
 }
+
 
 m.__reset__ = function() {
   // _modules = {}                   // resets module state
   //  or                             // only for testing purposes
-  // ModuleStore.reset();            // depends on your implementation
+  ModuleStore.reset();              // depends on your implementation
+  Module.prototype._cache = {};
 }
-
 
 module.exports = m;
