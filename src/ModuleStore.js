594e7a16b54ce86e47345f30d69b380b1bc68eba
dennisreveni <dennisreveni@rambler.ru>
2015-03-27 23:06:55 +0200

    Update ModuleStore.js

--- src/ModuleStore.js
+++ src/ModuleStore.js
@@ -11,15 +11,19 @@ function ModuleStore() {
 }
 
 ModuleStore.prototype.addModule = function(name, moduleInstance) {
+	this.store[name] = moduleInstance;
 }
 
 ModuleStore.prototype.getModule = function(name) {
+	return this.store[name];
 }
 
 ModuleStore.prototype.reset = function() {
+	this.store = {};
 }
 
 ModuleStore.prototype.contains = function(name) {
+	return name in this.store;
 }
 
 module.exports = ModuleStore;
